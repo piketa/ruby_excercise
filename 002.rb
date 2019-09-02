d7990bcda45aca68ddf8e65535010d87236cb93a
NorrisMatsui <norris@NorrisMatsuis-MacBook-Pro.local>
2019-09-03 08:59:59 +0900

    Update 002.rb and 003.rb

--- 002.rb
+++ 002.rb
@@ -1,8 +1,15 @@
-num = 1
 sum = 0
-while num <= 100
-  sum += num
-  num += 1
-end
-puts sum
+(1..100).each {|i| sum += i}
+p sum
+
+
+# eachメソッドを使用しないパターン
+#
+# num = 1
+# sum = 0
+# while num <= 100
+#   sum += num
+#   num += 1
+# end
+# puts sum
 
--- 003.rb
+++ 003.rb
@@ -1,10 +1,17 @@
-choco = 1
-min = 0
-
-while min <= 10
-  choco *= 2
-  min += 1
-end
-
+choco = 1  
+10.times{choco *= 2}
 puts choco
+
+
+# timesメソッドを使用しないパターン
+#
+# choco = 1
+# min = 1
+# 
+# while min <= 10
+#   choco *= 2
+#   min += 1
+# end
+# 
+# puts choco
 
